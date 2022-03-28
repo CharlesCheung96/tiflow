8979c4b4521f4319fdd11b47629f28d460e4c637
CharlesCheung <xiaoshuaizx@gmail.com>
2022-03-28 19:12:34 +0800

    test

--- cmd/scripts/main_test.go
+++ cmd/scripts/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"go.uber.org/goleak"
@@ -8,10 +9,19 @@ import (
 
 func TestMainParallel(t *testing.T) {
 	defer goleak.VerifyNone(t)
-	for pa:=1; pa<
-	initUpstream("test_2_million")
-	execInsert(1024)
-	close()
+	for pa := 1; pa <= 20480; {
+		fmt.Printf("========================Para: %v========================\n", pa)
+		initUpstream("test_2_million")
+		execInsert(pa)
+		close()
+		if pa < 1024 {
+			pa *= 4
+		} else if pa == 8192 {
+			pa = 10240
+		} else if pa >= 1024 {
+			pa *= 2
+		}
+	}
 }
 
 func TestMainBatch(t *testing.T) {
