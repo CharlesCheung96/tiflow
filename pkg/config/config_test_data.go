ca453026abb4d2950772aebb91bc50e5787c5dc6
CharlesCheung <xiaoshuaizx@gmail.com>
2024-01-03 13:42:23 +0800

    add metrics

--- pkg/config/config_test_data.go
+++ pkg/config/config_test_data.go
@@ -21,6 +21,11 @@ const (
   "force-replicate": true,
   "check-gc-safe-point": true,
   "enable-sync-point": false,
+<<<<<<< HEAD
+=======
+  "enable-table-monitor": false,
+  "bdr-mode": false,
+>>>>>>> 8fc0bff9c (Update metrics.go)
   "sync-point-interval": 600000000000,
   "sync-point-retention": 86400000000000,
   "filter": {
@@ -175,6 +180,7 @@ const (
   "force-replicate": true,
   "check-gc-safe-point": true,
   "enable-sync-point": false,
+  "enable-table-monitor": false,
   "bdr-mode": false,
   "sync-point-interval": 600000000000,
   "sync-point-retention": 86400000000000,
@@ -248,6 +254,7 @@ const (
   "force-replicate": true,
   "check-gc-safe-point": true,
   "enable-sync-point": false,
+  "enable-table-monitor": false,
   "bdr-mode": false,
   "sync-point-interval": 600000000000,
   "sync-point-retention": 86400000000000,
